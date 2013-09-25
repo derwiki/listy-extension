3bbb60751eb5ca67d0fdd217135ce01447a798e7
Adam Derewecki <derewecki@gmail.com>
2013-09-25 14:15:51 -0700

    Kicking it

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
-chrome.pageAction.onClicked.addListener(function() {
-  runtime.sendMessage();
+function listener(id, info, tab) {
+  if (tab.url.toLowerCase().indexOf("apartmentlist.com") === -1) {
+    console.log('not here');
+  } else {
+    console.log('invoking pageAction');
+    chrome.pageAction.show(tab.id);
+    chrome.tabs.sendMessage();
+  }
+}
+
+chrome.tabs.onUpdated.addListener(listener);
+chrome.pageAction.onClicked.addListener(function(tab) {
+    chrome.pageAction.show(tab.id);
 });
--- ldp.js
+++ ldp.js
@@ -1,8 +1,9 @@
 chrome.runtime.onMessage.addListener(function() {
+  alert('foo')
   $('#full_name').val('Test Test');
   $('#email').val('test@test.com');
   $('#phone').val('216-368-3457');
-  $('#message').val('Looking to move soon!');
+  $('#message').val('I am Looking to move soon!');
   var tmd = new Date(Date.now() + 3600 * 24 * 1000 * 3);
   var dateString = [
      tmd.getMonth() < 10 ? '0'+(1+tmd.getMonth()).toString() : tmd.getMonth(),
