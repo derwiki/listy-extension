4331028f164891b76061a6f28e29b586b0e59c68
Adam Derewecki <derewecki@gmail.com>
2013-09-25 14:55:39 -0700

    Only executes on button press

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 function listener(id, info, tab) {
   if (tab.url.toLowerCase().indexOf("apartmentlist.com") !== -1) {
     chrome.pageAction.show(tab.id);
-    chrome.tabs.executeScript(null, { "file": "ldp.js" });
   }
 }
 chrome.tabs.onUpdated.addListener(listener);
 chrome.pageAction.onClicked.addListener(function(tab) {
-    chrome.pageAction.show(tab.id);
+    chrome.tabs.executeScript(null, { "file": "ldp.js" });
 });
